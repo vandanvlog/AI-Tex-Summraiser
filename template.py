0abe6b5c06977f13e4f926df9dad253143a8d5f4
sage Academy <vandanprajapati2276@gmail.com>
2025-05-10 12:41:39 +0100

    project structure by creating fils and foldes

---
+++ template.py
@@ -0,0 +1,50 @@
+import os
+from pathlib import Path
+import logging
+
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s')
+
+project_name = "ai_text_summariser"
+
+list_fo_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/logging/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.ymal",
+    "app.py",
+    "main.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "research/trails.ipynb"
+
+
+] 
+
+
+for filepath in list_fo_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating direcatory: {filedir} for this the file {filename} ")
+
+    if (not os.path.exists(filepath)) or  (os.path.exists(filepath) == 0):
+        with open(filepath,'w') as f:
+            pass
+            logging.info(f"creting empty file: {filepath}")
+
+
+    else:
+        logging.info(f"{filename} is already exist ")
+        
