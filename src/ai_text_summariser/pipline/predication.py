09dc564ea4c0e8ce649bad77dc4ab6eb41981b4e
sage Academy <vandanprajapati2276@gmail.com>
2025-06-11 15:02:34 +0100

    requirement.txt cleaned up

--- src/ai_text_summariser/pipline/predication.py
+++ src/ai_text_summariser/pipline/predication.py
@@ -2,7 +2,6 @@ from ai_text_summariser.config.configuration import ConfigurationManager
 from transformers import AutoTokenizer
 from transformers import pipeline
 
-
 class PredictionPipeline:
     def __init__(self):
         self.config = ConfigurationManager().get_model_evaluation_config()
